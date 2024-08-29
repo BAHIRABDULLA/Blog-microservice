d8608fa2456435f3d7b84b85e1b1ac06ed7f3bc3
BAHIRABDULLA <bahirk880@gmail.com>
2024-08-29 14:34:09 +0530

    post model implimented

---
+++ backend/services/postService/src/controllers/createController.ts
@@ -0,0 +1,10 @@
+import { Request,Response } from "express"
+
+export const createPost = (req:Request,res:Response)=>{
+    try {
+        
+    } catch (error) {
+        console.error('Error updating data',error);
+        
+    }
+}
